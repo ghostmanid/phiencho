4a9a15db4dc5f08f1b45ab3a8f3a8ef0c8ddc0a7
Trung <trinhvantrung.it@gmail.com>
2015-07-14 21:22:08 +0700

    Get info  sap thanh cong

--- app/Http/Controllers/DomController.php
+++ app/Http/Controllers/DomController.php
@@ -20,33 +20,82 @@ class DomController extends Controller
         $site ='http://canthoinfo.com';
         $client = new Client();
         $crawler = $client->request('GET', $site);
-        $data=  array();
-        $j=0;
         $crawler = $crawler->filter('table > tr > td > table');
-        $crawler= $crawler->filter('tr')->eq(4);
+        $crawler = $crawler->filter('tr')->eq(4);
         $crawler = $crawler->filter('td>table>tr>td>table')->eq(3);
         $crawler = $crawler->filter('tr>td>table');
-        $tongsorecord =  (round(count($crawler)/3)); 
-       // for ($i=0; $i < 92 ; $i=$i+3) { 
-            print $crawler->eq()->filter('a')->attr('href');
-       // }
-        // $data1 = array();
-        // $j =0;
-        // for ($i=37; $i < count($crawler_link)-5 ; $i++) { 
-              
-        //       $data1[$j] = $crawler_link[$i];
-        //       $j++;
-        // }
-        // $data = array_unique($data1);
-        // echo "<pre>";
-        // print_r($data);
-        // echo "</pre>";
+        // link 
+        $data_link = $crawler->filter('tr>td>a')->extract('href');
+        
+        $data_link = $this->xoaPhanTu($data_link);
+                
+        //vitrituyendung
+        $arr_vitri = array();
+        $j=0;
+        for ($i=1; $i < count($crawler) ; $i=$i+3) { 
+            
+             $data_vitri= $crawler->eq($i)->filter('tr>td');
+             $data_vitri= $data_vitri->filter('font')->extract('_text');
+             $arr_vitri[$j] = $data_vitri;
+            $j++;
+             
+        }
+        
 
-        // $crawler_vitri = $crawler->filter('body table   table   table  table   table table  tr  td b font')->extract('_text');
-        // echo "<pre>";
-        // print_r($crawler_vitri);
-        // echo "</pre>";
-    }   
+        $data_thongtin = array();
+        for ($i=0; $i < count($data_link); $i++) { 
+            
+            $data_vt = '';
+                      
+            for ($j=2; $j <count($arr_vitri[$i])-2; $j++) { 
+                     $data_vt = trim($data_vt.$arr_vitri[$i][$j].',');
+                }
+            $data_thongtin[$i] = array(
+                'url' => $site.$data_link[$i],
+                'TenCty'=> $arr_vitri[$i][0],
+                'diachi'=> '',    
+                'vitri' =>   $data_vt,
+                'hanop'=> '???',
 
+            );
+
+        }
+        echo "<pre>";
+             print_r($data_thongtin) ;
+                echo "</pre>";
+            
+           
+        
+            
+                 
+        
+       
+    }
+
+/**
+* xoa phan tu trung nhau trong mang
+*/
+    private function xoaPhanTu($array)
+    {   
+        $data = array_unique($array);
+        foreach ($data as $key => $value) 
+        {
+                
+          if ('' == trim($value)) 
+          {
+            unset($data[$key]);
+          }
+
+        } //loai bo phan tu trung nhau 
+
+        // chay la key trong phan tu
+        $i=0;
+        $kq = array();
+        foreach ($data as $key => $value) {
+           $kq[$i] = $value;
+           $i++;
+        }
+        return $kq;
+    }
    
 }
